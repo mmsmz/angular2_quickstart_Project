d6355072486a5ebfc341f3e636799fc20621fa18
arafath <marafath07@gmail.com>
2019-10-13 16:44:16 +0530

    styling the employee component

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -2,7 +2,9 @@ import {Component} from '@angular/core';
 
 @Component({
     selector : 'my-employee',
-    templateUrl : 'app/employee/employee.component.html'
+    templateUrl : 'app/employee/employee.component.html',
+    styleUrls: ['app/employee/employee.component.css']
+
 })
 export class EmployeeComponent{
         firstname : string = 'Tom';
