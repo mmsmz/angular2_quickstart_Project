00ecf2668274457d154b692b6d8431405d03431d
arafath <marafath07@gmail.com>
2019-10-16 00:43:01 +0530

    Class Binding

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -8,9 +8,12 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
+        this.classesToApply = 'italicsClass boldClass';
+        this.applyBoldClass = false;
+        this.applyitalicClass = false;
         //  pageHeader : string = "Employee Details";
         // imagePath : string = '20180518/sm_5aff6089d3e02.png';
-        this.isDisabled = false;
+        // isDisabled: boolean = false;
         // badHtml: String = 'hello <script>alert("Hacked");</script> World'
         // firstName : string = 'Tom';
         // lastName : string = 'Sammy';
@@ -18,12 +21,19 @@ var AppComponent = (function () {
         //     return this.firstName + " " + this.lastName;
         // }
     }
+    AppComponent.prototype.addClasses = function () {
+        var classes = {
+            boldClass: this.applyBoldClass,
+            italicsClass: this.applyitalicClass
+        };
+        return classes;
+    };
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "\n             <div>\n                <!-- <h1> {{'Page Header ' + pageHeader}} </h1> -->\n                <!-- <h1> {{10+20+30}} </h1> -->\n                \n                <!-- using ternary operator-->\n                \n                <!--\n                <h1> {{ pageHeader ? pageHeader : 'No header'}} </h1> \n                <img src='https://png.pngtree.com/element_our/sm/{{imagePath}}' />\n                <h1> {{ getFullName()}} </h1> \n                \n                <button [disabled]='isDisabled'>Click Me</button>\n                <button disabled='{{isDisabled}}'>Click Me</button>\n                <span bind-innerHtml='pageHeader'></span>\n                <div [innerHtml]='badHtml'>{{badHtml}}</div>-->\n                \n                \n                <my-employee></my-employee>\n                <!-- <input id='inputId' type='text' value='Tom'>-->\n\n\n             </div>"
+        template: "\n             <div>\n                <!-- <h1> {{'Page Header ' + pageHeader}} </h1> -->\n                <!-- <h1> {{10+20+30}} </h1> -->\n                \n                <!-- using ternary operator-->\n                \n                <!--\n                <h1> {{ pageHeader ? pageHeader : 'No header'}} </h1> \n                <img src='https://png.pngtree.com/element_our/sm/{{imagePath}}' />\n                <h1> {{ getFullName()}} </h1> \n                \n                <button [disabled]='isDisabled'>Click Me</button>\n                <button disabled='{{isDisabled}}'>Click Me</button>\n                <span bind-innerHtml='pageHeader'></span>\n                <div [innerHtml]='badHtml'>{{badHtml}}</div>\n                \n                <my-employee></my-employee>\n                <input id='inputId' type='text' value='Tom'>-->\n\n                <button class=\"colorClass\" [class]='classesToApply'>My Button</button>\n                <br/><br/>\n                <button class=\"colorClass italicsClass boldClass\" [class.boldClass]='applyBoldClass'>My Button</button>\n                <br/><br/>\n                <button class=\"colorClass\" [ngClass]='addClasses()'>My Button</button>\n                </div>"
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,20 +17,37 @@ import {Component}  from "@angular/core";
                 <button [disabled]='isDisabled'>Click Me</button>
                 <button disabled='{{isDisabled}}'>Click Me</button>
                 <span bind-innerHtml='pageHeader'></span>
-                <div [innerHtml]='badHtml'>{{badHtml}}</div>-->
-                
+                <div [innerHtml]='badHtml'>{{badHtml}}</div>
                 
                 <my-employee></my-employee>
-                <!-- <input id='inputId' type='text' value='Tom'>-->
+                <input id='inputId' type='text' value='Tom'>-->
 
-
-             </div>`
+                <button class="colorClass" [class]='classesToApply'>My Button</button>
+                <br/><br/>
+                <button class="colorClass italicsClass boldClass" [class.boldClass]='applyBoldClass'>My Button</button>
+                <br/><br/>
+                <button class="colorClass" [ngClass]='addClasses()'>My Button</button>
+                </div>`
 })
 export class AppComponent {
   
+  classesToApply : string = 'italicsClass boldClass';
+  applyBoldClass : boolean = false;
+  applyitalicClass : boolean = false;
+
+  addClasses(){
+      let classes = {
+          boldClass:this.applyBoldClass, 
+          italicsClass:this.applyitalicClass
+      };
+      return classes;
+  }
+
   //  pageHeader : string = "Employee Details";
   // imagePath : string = '20180518/sm_5aff6089d3e02.png';
-  isDisabled: boolean = false;
+  // isDisabled: boolean = false;
+  
+
   // badHtml: String = 'hello <script>alert("Hacked");</script> World'
 
   // firstName : string = 'Tom';
